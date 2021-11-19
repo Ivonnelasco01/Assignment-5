aeb4b0def96dd6beb771ef52143bdee349ed80d1
Ivonnelasco01 <92631714+Ivonnelasco01@users.noreply.github.com>
2021-11-19 09:23:42 +0800

    description

--- program 3.py
+++ program 3.py
@@ -1,5 +1,13 @@
 def life_stage():
     age = int(input("Age: "))
-    if age > -1 and age <= 12
+
+    if age >= -1 and age <= 12:
         print("Kid")
+    elif age >= 13 and age <= 17:
+        print("Teen")
+    elif age == 18:
+        print("Debut")
+    elif age >= 19:
+        print("Adult")
+
 lifeStage = life_stage()
