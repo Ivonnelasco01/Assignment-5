be706ca56c9f4660c3cceb18cd8007218f83777c
Ivonnelasco01 <92631714+Ivonnelasco01@users.noreply.github.com>
2021-11-19 09:00:57 +0800

    description

--- program 2.py
+++ program 2.py
@@ -2,6 +2,11 @@ def low_num():
     num1 = int(input("Enter 1st number: "))
     num2 = int(input("Enter 2nd number: "))
     num3 = int(input("Enter 3rd number: "))
-
+    min = num1
+    if num2 < min:
+        min = num2
+    elif num3 < min:
+        min = num3
+    print(min,"is the smallest number")
 lowestNumber = low_num()
 
