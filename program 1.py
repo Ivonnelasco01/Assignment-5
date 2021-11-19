46da7ab8a0cd65e8a83d2398ed6e7d66eb7531a2
Ivonnelasco01 <92631714+Ivonnelasco01@users.noreply.github.com>
2021-11-20 05:56:08 +0800

    description

--- program 1.py
+++ program 1.py
@@ -15,31 +15,31 @@ def Grade_Percentage():
         if Percentage >= 97 and Percentage <= 100:
             print("Grade/Mark: 1.0")
             print("Description: Excellent")
-        elif Percentage >= 94 and Percentage <= 96:
+        elif Percentage >= 94 and Percentage <= 96.9:
             print("Grade/Mark: 1.25")
             print("Description: Excellent")
-        elif Percentage >= 91 and Percentage <= 93:
+        elif Percentage >= 91 and Percentage <= 93.9:
             print("Grade/Mark: 1.5")
             print("Description: Very Good")
-        elif Percentage >= 88 and Percentage <= 90:
+        elif Percentage >= 88 and Percentage <= 90.9:
             print("Grade/Mark: 1.75")
             print("Description: Very Good")
-        elif Percentage >= 85 and Percentage <= 87:
+        elif Percentage >= 85 and Percentage <= 87.9:
             print("Grade/Mark: 2.0")
             print("Description: Good")
-        elif Percentage >= 82 and Percentage <= 84:
+        elif Percentage >= 82 and Percentage <= 84.9:
             print("Grade/Mark: 2.25")
             print("Description: Good")
-        elif Percentage >= 79 and Percentage <= 81:
+        elif Percentage >= 79 and Percentage <= 81.9:
             print("Grade/Mark: 2.5")
             print("Description: Satisfactory")
-        elif Percentage >= 76 and Percentage <= 78:
+        elif Percentage >= 76 and Percentage <= 78.9:
             print("Grade/Mark: 2.75")
             print("Description: Satisfactory")
-        elif Percentage == 75:
+        elif Percentage >= 75 and Percentage <= 75.9:
             print("Grade/Mark: 3.0")
             print("Description: Passing")
-        elif Percentage >= 65 and Percentage <= 74:
+        elif Percentage >= 65 and Percentage <= 74.9:
             print("Grade/Mark: 5.0")
             print("Description: Failure")
 
--- program 2.py
+++ program 2.py
@@ -3,10 +3,11 @@ def low_num():
     num2 = int(input("Enter 2nd number: "))
     num3 = int(input("Enter 3rd number: "))
     min = num1
-    if num2 < min:
+    if min < num2 and min < num3 :
+        min = min
+    elif num2 < min and num2 < num3:
         min = num2
-    elif num3 < min:
+    elif num3 < min and num3 < num2:
         min = num3
     print(min,"is the smallest number")
 lowestNumber = low_num()
-
