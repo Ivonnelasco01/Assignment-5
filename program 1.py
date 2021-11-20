5f9195f86e905f2edf6a48b61b5718cc7c6278b8
Ivonnelasco01 <92631714+Ivonnelasco01@users.noreply.github.com>
2021-11-20 20:22:26 +0800

    description

--- program 1.py
+++ program 1.py
@@ -1,5 +1,6 @@
+import math
 def Grade_Percentage():
-    percentage = (input("Enter your Grade Percentage here: "))
+    percentage = input("Enter your Grade Percentage here: ")
 
     if percentage == "Inc.":
         print("Grade/Mark: Inc.")
@@ -12,34 +13,45 @@ def Grade_Percentage():
         print("Dropped")
     else:
         Percentage = float(percentage)
-        if Percentage >= 97 and Percentage <= 100:
+
+        if Percentage >= 96.6 and Percentage <= 100:
+            print("Input Grade: ", round(Percentage))
             print("Grade/Mark: 1.0")
             print("Description: Excellent")
-        elif Percentage >= 94 and Percentage <= 96.9:
+        elif Percentage >= 93.5 and Percentage <= 96.5:
+            print("Input Grade: ", round(Percentage))
             print("Grade/Mark: 1.25")
             print("Description: Excellent")
-        elif Percentage >= 91 and Percentage <= 93.9:
+        elif Percentage >= 90.6 and Percentage <= 93.4:
+            print("Input Grade: ", round(Percentage))
             print("Grade/Mark: 1.5")
             print("Description: Very Good")
-        elif Percentage >= 88 and Percentage <= 90.9:
+        elif Percentage >= 87.5 and Percentage <= 90.5:
+            print("Input Grade: ", round(Percentage))
             print("Grade/Mark: 1.75")
             print("Description: Very Good")
-        elif Percentage >= 85 and Percentage <= 87.9:
+        elif Percentage >= 84.6 and Percentage <= 87.4:
+            print("Input Grade: ", round(Percentage))
             print("Grade/Mark: 2.0")
             print("Description: Good")
-        elif Percentage >= 82 and Percentage <= 84.9:
+        elif Percentage >= 82.5 and Percentage <= 84.5:
+            print("Input Grade: ", round(Percentage))
             print("Grade/Mark: 2.25")
             print("Description: Good")
-        elif Percentage >= 79 and Percentage <= 81.9:
+        elif Percentage >= 78.6 and Percentage <= 81.4:
+            print("Input Grade: ", round(Percentage))
             print("Grade/Mark: 2.5")
             print("Description: Satisfactory")
-        elif Percentage >= 76 and Percentage <= 78.9:
+        elif Percentage >= 75.5 and Percentage <= 78.5:
+            print("Input Grade: ", round(Percentage))
             print("Grade/Mark: 2.75")
             print("Description: Satisfactory")
-        elif Percentage >= 75 and Percentage <= 75.9:
+        elif Percentage >= 74.6 and Percentage <= 75.4:
+            print("Input Grade: ", round(Percentage))
             print("Grade/Mark: 3.0")
             print("Description: Passing")
-        elif Percentage >= 65 and Percentage <= 74.9:
+        elif Percentage >= 65 and Percentage <= 74.5:
+            print("Input Grade: ", round(Percentage))
             print("Grade/Mark: 5.0")
             print("Description: Failure")
 
